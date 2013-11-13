f9e50a3d011459f6acad1f9bd235579841c6cc0c
Steve Foster <srfoster@speakeasy.net>
2013-11-13 12:11:51 -0800

    inital commit after setting up new application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kidstack::Application.config.secret_key_base = 'abc507f28124d7798a5711491242efc0cd0e167b26bf223fd1d51ad120a9683dfda03628f366b94e53c643ca947c015d6084bea6e8308e047b1cf6e63a835f40'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kidstack::Application.config.session_store :cookie_store, key: '_kidstack_session'
