3d9d421cc398d67681039d4fd28e88ac1f938003
emapstone <ehardy@2025labs.com>
2014-03-11 13:36:46 -0700

    cleaning up controls

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,17 @@
+var myVar=setInterval(function(){myTimer()},1000);
+var myVar=setInterval(function(){Banana()},3000);
 
-	var myVar=setInterval(function(){myTimer()},1000);
-
-	function myTimer()
-	{
+function myTimer()
+{
 	var d=new Date();
 	var t=d.toLocaleTimeString();
 	document.getElementById("demo").innerHTML=t;
-	}
+}
+
+
+function Banana()
+{
+	
+	document.getElementById("banana").innerHTML="Banana";
+}
+
