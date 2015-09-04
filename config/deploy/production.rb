db7185d8615aa0ad7dfe8e6a5f9e4e2e9d25154f
Melissa Liu <mliu@2025labs.com>
2015-09-04 12:59:32 -0700

    fixing wrong ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 set :domain, "www.2025kids.com"  # 
 
-role :app, "50.10.245.159"
-role :web, "50.10.245.159"
-role :worker, "50.10.245.159"
+role :app, "52.10.245.159"
+role :web, "52.10.245.159"
+role :worker, "52.10.245.159"
 
-server "50.10.245.159", :db, :primary=>true
+server "52.10.245.159", :db, :primary=>true
 
