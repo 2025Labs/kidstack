aadcbe4df1033f8002b062cd0148609695d49557
Melissa Liu <mliu@2025labs.com>
2015-09-04 12:48:04 -0700

    new ip addr for kids prod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
-set :domain, "www.kidstack.com"  # 
+set :domain, "www.2025kids.com"  # 
 
-role :app, "54.200.252.131"
-role :web, "54.200.252.131"
-role :worker, "54.200.252.131"
+role :app, "50.10.245.159"
+role :web, "50.10.245.159"
+role :worker, "50.10.245.159"
 
-server "54.200.252.131", :db, :primary=>true
+server "50.10.245.159", :db, :primary=>true
 
