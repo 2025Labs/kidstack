3016dd9a41ccbee8a80f69e48ac778e5b2a4b819
Melissa Liu <mliu@2025labs.com>
2016-07-15 11:08:43 -0700

    quiz bug fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,7 +7,7 @@ class Question < ActiveRecord::Base
     def catnext
 
       if topic_id == nil
-        catagory.questions.where("id > ?", id).order("question_num ASC").first
+        catagory.questions.where("id > ?", id).order("id ASC").first
       else
         topic.questions.where("id > ?", id).order("id ASC").first
       end
